65a039f62531f2cdf13d765cbb44601d2217cb1c
Vasigaran R <123574979+vasi002@users.noreply.github.com>
2025-09-26 11:36:05 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -33,18 +33,13 @@ function About() {
           <div className="timeline-content">
             <h4>Software Engineer at Altaradius Technologies Pvt Ltd,Chennai</h4>
             <ul>
-             <li>Joined as an Intern (Jun–Dec 2024) and transitioned to a full-time Software
-Engineer role in Dec 2024.</li>
-<li> Performed end-to-end software development and testing activities across
-multiple projects.</li>
-<li> Executed both manual and automated testing using Selenium, and prepared
-comprehensive test cases.</li>
-<li> Worked with Microsoft SQL for test data validation and backend verification.</li>
-<li> Developed responsive and user-friendly web application interfaces using
-HTML, CSS, and Bootstrap.</li>
-<li> Identified, documented,and tracked software defects using JIRA, ensuring
-timely resolution.</li>
-                         </ul>
+                <li>Joined as an Intern (Jun–Dec 2024) and transitioned to a full-time Software Engineer role in Dec 2024.</li>
+                <li> Performed end-to-end software development and testing activities across multiple projects.</li>
+                <li> Executed both manual and automated testing using Selenium, and prepared comprehensive test cases.</li>
+                <li> Worked with Microsoft SQL for test data validation and backend verification.</li>
+                <li> Developed responsive and user-friendly web application interfaces using HTML, CSS, and Bootstrap.</li>
+                <li> Identified, documented,and tracked software defects using JIRA, ensuring timely resolution.</li>
+              </ul>
           </div>
         </div>
       </motion.div>
